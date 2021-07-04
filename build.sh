04d806c9c2ad73be7a49d6a0472c6b405662dddd
xiaoming <2014500726@smail.xtu.edu.cn>
2021-07-04 20:39:16 +0800

    修复编译脚本错误

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 
 cd qemu-6.0.0
-if [ ! -d "$SHELL_FOLDER" ]; then  
+if [ ! -d "$SHELL_FOLDER/output/qemu" ]; then  
 ./configure --prefix=$SHELL_FOLDER/output/qemu  --target-list=riscv64-softmmu --enable-gtk  --enable-virtfs --disable-gio
 fi  
 make -j16
