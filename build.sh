0704febed98f365def98337bd0305570486d1c51
xiaoming <2014500726@smail.xtu.edu.cn>
2021-07-04 11:57:56 +0800

    添加qemu简易编译脚本

---
+++ build.sh
@@ -0,0 +1,9 @@
+SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+
+cd qemu-6.0.0
+if [ ! -d "$SHELL_FOLDER" ]; then  
+./configure --prefix=$SHELL_FOLDER/output/qemu  --target-list=riscv64-softmmu --enable-gtk  --enable-virtfs --disable-gio
+fi  
+make -j16
+make install
+cd ..
