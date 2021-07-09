b65dc8b710e5643c10d3075a47c38895a9dc4b83
xiaoming <2014500726@smail.xtu.edu.cn>
2021-07-09 23:42:59 +0800

    添加uboot编译脚本并加载引到进入uboot环境

--- build.sh
+++ build.sh
@@ -42,6 +42,23 @@ $CROSS_PREFIX-gcc -nostartfiles -T./link.lds -Wl,-Map=$SHELL_FOLDER/output/trust
 $CROSS_PREFIX-objcopy -O binary -S $SHELL_FOLDER/output/trusted_domain/trusted_fw.elf $SHELL_FOLDER/output/trusted_domain/trusted_fw.bin
 $CROSS_PREFIX-objdump --source --demangle --disassemble --reloc --wide $SHELL_FOLDER/output/trusted_domain/trusted_fw.elf > $SHELL_FOLDER/output/trusted_domain/trusted_fw.lst
 
+# 编译uboot
+if [ ! -d "$SHELL_FOLDER/output/uboot" ]; then  
+mkdir $SHELL_FOLDER/output/uboot
+fi  
+cd $SHELL_FOLDER/u-boot-2021.07
+make CROSS_COMPILE=/opt/riscv64--glibc--bleeding-edge-2020.08-1/bin/riscv64-linux- qemu-riscv64_smode_defconfig
+make CROSS_COMPILE=/opt/riscv64--glibc--bleeding-edge-2020.08-1/bin/riscv64-linux- -j16
+cp $SHELL_FOLDER/u-boot-2021.07/u-boot $SHELL_FOLDER/output/uboot/u-boot.elf
+cp $SHELL_FOLDER/u-boot-2021.07/u-boot.map $SHELL_FOLDER/output/uboot/u-boot.map
+cp $SHELL_FOLDER/u-boot-2021.07/u-boot.bin $SHELL_FOLDER/output/uboot/u-boot.bin
+$CROSS_PREFIX-objdump --source --demangle --disassemble --reloc --wide $SHELL_FOLDER/output/uboot/u-boot.elf > $SHELL_FOLDER/output/uboot/u-boot.lst
+
+
+# 生成uboot.dtb
+cd $SHELL_FOLDER/dts
+dtc -I dts -O dtb -o $SHELL_FOLDER/output/uboot/quard_star_uboot.dtb quard_star_uboot.dts
+
 # 合成firmware固件
 if [ ! -d "$SHELL_FOLDER/output/fw" ]; then  
 mkdir $SHELL_FOLDER/output/fw
@@ -51,7 +68,9 @@ rm -rf fw.bin
 dd of=fw.bin bs=1k count=32k if=/dev/zero
 dd of=fw.bin bs=1k conv=notrunc seek=0 if=$SHELL_FOLDER/output/lowlevelboot/lowlevel_fw.bin
 dd of=fw.bin bs=1k conv=notrunc seek=512 if=$SHELL_FOLDER/output/opensbi/quard_star_sbi.dtb
+dd of=fw.bin bs=1k conv=notrunc seek=1K if=$SHELL_FOLDER/output/uboot/quard_star_uboot.dtb
 dd of=fw.bin bs=1k conv=notrunc seek=2K if=$SHELL_FOLDER/output/opensbi/fw_jump.bin
 dd of=fw.bin bs=1k conv=notrunc seek=4K if=$SHELL_FOLDER/output/trusted_domain/trusted_fw.bin
+dd of=fw.bin bs=1k conv=notrunc seek=8K if=$SHELL_FOLDER/output/uboot/u-boot.bin
 
 cd $SHELL_FOLDER
--- lowlevelboot/startup.s
+++ lowlevelboot/startup.s
@@ -49,13 +49,33 @@ _start:
 	load_data a0,a1,a2
 
 	//load trusted_fw.bin
-	//[0x20400000:0x20800000] --> [0x80200000:0x80600000]
+	//[0x20400000:0x20800000] --> [0xb0000000:0xb0400000]
     li		a0,	0x204
 	slli	a0,	a0, 20      //a0 = 0x20400000
     li		a1,	0xb00
 	slli	a1,	a1, 20      //a1 = 0xb0000000
     li		a2,	0xb04
 	slli	a2,	a2, 20      //a2 = 0xb0400000
+	load_data a0,a1,a2
+
+	//load qemu_uboot.dtb
+	//[0x20100000:0x20180000] --> [0x82000000:0x82080000]
+    li		a0,	0x201
+	slli	a0,	a0, 20       //a0 = 0x20100000
+	li		a1,	0x820
+	slli	a1,	a1, 20       //a1 = 0x82000000
+    li		a2,	0x8208
+	slli	a2,	a2, 16       //a2 = 0x82080000
+	load_data a0,a1,a2
+
+	//load u-boot.bin
+	//[0x20800000:0x20C00000] --> [0x80200000:0x80600000]
+    li		a0,	0x208
+	slli	a0,	a0, 20      //a0 = 0x20800000
+    li		a1,	0x802
+	slli	a1,	a1, 20      //a1 = 0x80200000
+    li		a2,	0x806
+	slli	a2,	a2, 20      //a2 = 0x80600000
 	load_data a0,a1,a2
 
     csrr    a0, mhartid
