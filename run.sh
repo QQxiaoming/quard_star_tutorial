a36cf9d3098b85dc0fbcde479c87da2b464eae5c
xiaoming <2014500726@smail.xtu.edu.cn>
2021-07-11 12:36:29 +0800

    修改运行脚本

--- run.sh
+++ run.sh
@@ -1,5 +1,53 @@
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
-DEFAULT_VC="1280x720"
+
+VC=\
+"1920x1080 | \
+1600x900 | \
+1280x960 | \
+1280x720 | \
+1024x768 | \
+960x640 | \
+960x540 | \
+800x600 | \
+800x480 | \
+640x480 | \
+640x340"
+
+if [ $# != 1 ] ; then
+	if [ $# != 2 ] ; then
+		echo "usage $0 [graphic | nographic] [$VC]"
+		exit 1
+	fi
+fi
+
+case "$1" in
+graphic)
+	DEFAULT_VC=$2
+	if [ $# != 2 ] ; then
+		DEFAULT_VC="1280x720"
+	fi
+	GRAPHIC_PARAM="--display gtk --serial vc:$DEFAULT_VC --serial vc:$DEFAULT_VC --serial vc:$DEFAULT_VC --parallel none --monitor vc:$DEFAULT_VC"
+	
+	WIDTH="$(echo $DEFAULT_VC | sed 's/\(.*\)x\(.*\)/\1/g')"
+	HEIGHT="$(echo $DEFAULT_VC | sed 's/\(.*\)x\(.*\)/\2/g')"
+	ROWS="$(echo $WIDTH / 8 |bc)"
+	COLS="$(echo $HEIGHT / 16 |bc)"
+	DEFAULT_V=":vn:$COLS""x""$ROWS:"
+	;;
+nographic)
+	DEFAULT_VN="$(stty size | sed '/ \+/s//x/g')" 
+    GRAPHIC_PARAM="-nographic --parallel none"
+	DEFAULT_V=":vn:$DEFAULT_VN:"
+    ;;
+--help)
+	echo "usage $0 [graphic | nographic] [$VC]"
+	exit 0
+	;;
+*)
+	echo "usage $0 [graphic | nographic] [$VC]"
+	exit 1	
+	;;
+esac
 
 $SHELL_FOLDER/output/qemu/bin/qemu-system-riscv64 \
 -M quard-star \
@@ -9,7 +57,6 @@ $SHELL_FOLDER/output/qemu/bin/qemu-system-riscv64 \
 -drive file=$SHELL_FOLDER/output/rootfs/rootfs.img,format=raw,id=hd0 \
 -device virtio-blk-device,drive=hd0 \
 -fw_cfg name="opt/qemu_cmdline",string="qemu_vc="$DEFAULT_V"" \
--nographic 
+$GRAPHIC_PARAM
 
 #-device virtio-gpu-device,id=video0,xres=1280,yres=720 \
-#--serial vc:$DEFAULT_VC --serial vc:$DEFAULT_VC --serial vc:$DEFAULT_VC --monitor vc:$DEFAULT_VC --parallel none
