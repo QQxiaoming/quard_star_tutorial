914c5f2f96dc778ac0648aa3cb8576406aa889af
xiaoming <2014500726@smail.xtu.edu.cn>
2021-08-08 20:54:28 +0800

    更新uboot配置

--- u-boot-2021.07/include/configs/qemu-quard-star.h
+++ u-boot-2021.07/include/configs/qemu-quard-star.h
@@ -1,55 +1,46 @@
 /* SPDX-License-Identifier: GPL-2.0+ */
-/*
- * Copyright (C) 2018, Bin Meng <bmeng.cn@gmail.com>
- */
 
 #ifndef __CONFIG_H
 #define __CONFIG_H
 
 #include <linux/sizes.h>
 
-#define CONFIG_SYS_SDRAM_BASE		0x80000000
-#define CONFIG_SYS_INIT_SP_ADDR		(CONFIG_SYS_SDRAM_BASE + SZ_2M)
-
-#define CONFIG_SYS_LOAD_ADDR		(CONFIG_SYS_SDRAM_BASE + SZ_2M)
-
+#define CONFIG_SYS_SDRAM_BASE		(0x80000000UL)
+#define CONFIG_SYS_INIT_SP_ADDR		(0xB0080000UL)
+#define CONFIG_SYS_LOAD_ADDR		(CONFIG_SYS_SDRAM_BASE)
 #define CONFIG_SYS_MALLOC_LEN		SZ_8M
-
-#define CONFIG_SYS_BOOTM_LEN		SZ_64M
-
-#define CONFIG_STANDALONE_LOAD_ADDR	0x80200000
+#define CONFIG_SYS_BOOTM_LEN		SZ_32M
+#define CONFIG_STANDALONE_LOAD_ADDR	(CONFIG_SYS_LOAD_ADDR)
 
 #define RISCV_MMODE_TIMERBASE		0x2000000
 #define RISCV_MMODE_TIMER_FREQ		1000000
-
 #define RISCV_SMODE_TIMER_FREQ		1000000
 
 /* Environment options */
-
 #ifndef CONFIG_SPL_BUILD
 #define BOOT_TARGET_DEVICES(func) \
-	func(QEMU, qemu, na) \
+	func(QEMU, qemu, na)          \
 	func(VIRTIO, virtio, 0)
 
 #include <config_distro_bootcmd.h>
 
-#define BOOTENV_DEV_QEMU(devtypeu, devtypel, instance) \
-	"bootcmd_qemu=" \
-		"if env exists kernel_start; then " \
+#define BOOTENV_DEV_QEMU(devtypeu, devtypel, instance)   \
+	"bootcmd_qemu="                                      \
+		"if env exists kernel_start; then "              \
 			"bootm ${kernel_start} - ${fdtcontroladdr};" \
 		"fi;\0"
 
 #define BOOTENV_DEV_NAME_QEMU(devtypeu, devtypel, instance) \
 	"qemu "
 
-#define CONFIG_EXTRA_ENV_SETTINGS \
-	"fdt_high=0xffffffffffffffff\0" \
+#define CONFIG_EXTRA_ENV_SETTINGS      \
+	"fdt_high=0xffffffffffffffff\0"    \
 	"initrd_high=0xffffffffffffffff\0" \
-	"kernel_addr_r=0x84000000\0" \
-	"fdt_addr_r=0x88000000\0" \
-	"scriptaddr=0x88100000\0" \
-	"pxefile_addr_r=0x88200000\0" \
-	"ramdisk_addr_r=0x88300000\0" \
+	"kernel_addr_r=0x80000000\0"       \
+	"fdt_addr_r=0x82000000\0"          \
+	"scriptaddr=0x88000000\0"          \
+	"pxefile_addr_r=0x88100000\0"      \
+	"ramdisk_addr_r=0x88200000\0"      \
 	BOOTENV
 #endif
 
