ac2ef6064fdd24a9cf1a3d8091877ca0d13b168d
xiaoming <2014500726@smail.xtu.edu.cn>
2021-08-18 13:20:59 +0800

    add fontconfig

---
+++ target_root_app/fontconfig-2.13.94/src/fcstdint.h
@@ -0,0 +1,9 @@
+#ifndef _FONTCONFIG_SRC_FCSTDINT_H
+#define _FONTCONFIG_SRC_FCSTDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "fontconfig 2.13.94"
+/* generated using gnu compiler gcc (GCC) 11.1.1 20210623 (Red Hat 11.1.1-6) */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
