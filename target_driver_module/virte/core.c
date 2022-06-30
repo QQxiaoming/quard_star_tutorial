053e25e5edb4f2ac87e1f4b748e1c42582b4068d
xiaoming <2014500726@smail.xtu.edu.cn>
2022-06-30 11:12:27 +0800

    module/virte:update code
    Signed-off-by: xiaoming <2014500726@smail.xtu.edu.cn>

--- target_driver_module/virte/core.c
+++ target_driver_module/virte/core.c
@@ -8,12 +8,10 @@
 
 struct virtual_eth_port {
 	struct net_device *netdev;
-	struct napi_struct	napi;
 	struct virtual_eth_port *partner_dev;
 	int	id;
 	struct work_struct work;
     spinlock_t lock;
-	bool closing;
 	struct sk_buff *tx_skb;
 	uint64_t tx_packets;
 	uint64_t tx_bytes;
@@ -27,21 +25,16 @@ static struct virtual_eth_port *vport[2];
 
 static netdev_tx_t send_packet(struct sk_buff *skb, struct net_device *dev)
 {
-	struct virtual_eth_port *priv = netdev_priv(dev);
+	struct virtual_eth_port *vport = netdev_priv(dev);
 	unsigned long flags;
-	bool closing;
 
 	netif_stop_queue(dev);
 
-	spin_lock_irqsave(&priv->lock, flags);
-	closing = priv->closing;
-	priv->tx_skb = skb;
-	spin_unlock_irqrestore(&priv->lock, flags);
+	spin_lock_irqsave(&vport->lock, flags);
+	vport->tx_skb = skb;
+	spin_unlock_irqrestore(&vport->lock, flags);
 
-	if (closing)
-		dev_kfree_skb(skb);
-	else
-		schedule_work(&priv->work);
+	schedule_work(&vport->work);
 
 	return NETDEV_TX_OK;
 }
@@ -49,52 +42,49 @@ static netdev_tx_t send_packet(struct sk_buff *skb, struct net_device *dev)
 static void get_stats64(struct net_device *netdev,
 			    		struct rtnl_link_stats64 *stats)
 {
-	struct virtual_eth_port *priv = netdev_priv(netdev);
-	stats->tx_packets = priv->tx_packets;
-	stats->tx_bytes = priv->tx_bytes;
-	stats->tx_errors = priv->tx_errors;
-	stats->rx_packets = priv->rx_packets;
-	stats->rx_bytes = priv->rx_bytes;
-	stats->rx_errors = priv->rx_errors;
-}
+	struct virtual_eth_port *vport = netdev_priv(netdev);
 
-//static int napi_poll(struct napi_struct *napi, int budget)
-//{
-//	pr_info("qqm:E %s\n",__func__);
-//	pr_info("qqm:X %s\n",__func__);
-//	return 0;
-//}
+	stats->tx_packets = vport->tx_packets;
+	stats->tx_bytes = vport->tx_bytes;
+	stats->tx_errors = vport->tx_errors;
+	stats->rx_packets = vport->rx_packets;
+	stats->rx_bytes = vport->rx_bytes;
+	stats->rx_errors = vport->rx_errors;
+}
 
 static void transmission_data(struct work_struct *work)
 {
-    struct virtual_eth_port *self_port = container_of(work, struct virtual_eth_port, work);
-	struct virtual_eth_port *target_port = self_port->partner_dev;
+    struct virtual_eth_port *self_vport = container_of(work, struct virtual_eth_port, work);
+	struct virtual_eth_port *target_vport = self_vport->partner_dev;
 	struct sk_buff *tx_skb;
 	struct sk_buff *rx_skb;
 	unsigned long flags;
 	unsigned int len;
 
-	spin_lock_irqsave(&self_port->lock, flags);
-	tx_skb = self_port->tx_skb;
-	spin_unlock_irqrestore(&self_port->lock, flags);
+	spin_lock_irqsave(&self_vport->lock, flags);
+	tx_skb = self_vport->tx_skb;
+	spin_unlock_irqrestore(&self_vport->lock, flags);
+
 	if (tx_skb) {
 		len = tx_skb->len;
-		//print_hex_dump(KERN_ERR, "", DUMP_PREFIX_NONE, 16, 1, (void *)tx_skb->data, len, 0);
-		rx_skb = netdev_alloc_skb(target_port->netdev,len);
+		rx_skb = netdev_alloc_skb(target_vport->netdev,len);
 		skb_put_data(rx_skb, tx_skb->data,len);
-		rx_skb->protocol = eth_type_trans(rx_skb, target_port->netdev);
+		rx_skb->protocol = eth_type_trans(rx_skb, target_vport->netdev);
 		netif_rx(rx_skb);
-		spin_lock_irqsave(&self_port->lock, flags);
-		self_port->tx_skb = NULL;
-		self_port->tx_packets++;
-		self_port->tx_bytes+=len;
-		spin_unlock_irqrestore(&self_port->lock, flags);
-		spin_lock_irqsave(&target_port->lock, flags);
-		target_port->rx_packets++;
-		target_port->rx_bytes+=len;
-		spin_unlock_irqrestore(&target_port->lock, flags);
+
+		spin_lock_irqsave(&self_vport->lock, flags);
+		self_vport->tx_skb = NULL;
+		self_vport->tx_packets++;
+		self_vport->tx_bytes+=len;
+		spin_unlock_irqrestore(&self_vport->lock, flags);
+
+		spin_lock_irqsave(&target_vport->lock, flags);
+		target_vport->rx_packets++;
+		target_vport->rx_bytes+=len;
+		spin_unlock_irqrestore(&target_vport->lock, flags);
+
 		dev_kfree_skb(tx_skb);
-		netif_wake_queue(self_port->netdev);
+		netif_wake_queue(self_vport->netdev);
 	}
 }
 
@@ -107,28 +97,30 @@ static const struct net_device_ops netdev_ops = {
 
 static struct virtual_eth_port *alloc_and_init_device(unsigned int id)
 {
-	struct virtual_eth_port *dev;
+	struct virtual_eth_port *vport;
 	struct net_device *netdev;
 
-	netdev = alloc_etherdev(sizeof(struct virtual_eth_port));
+	netdev = alloc_netdev_mqs(sizeof(struct virtual_eth_port), "veth%d", NET_NAME_ENUM,
+				ether_setup, 1, 1);
 	if (!netdev) {
 		return NULL;
 	}
-	dev = netdev_priv(netdev);
-	dev->netdev = netdev;
-	dev->id = id;
-	dev->tx_packets = 0;
-	dev->tx_bytes = 0;
-	dev->tx_errors = 0;
-	dev->rx_packets = 0;
-	dev->rx_bytes = 0;
-	dev->rx_errors = 0;
 
-    spin_lock_init(&dev->lock);
-   	INIT_WORK(&dev->work, transmission_data);
+	vport = netdev_priv(netdev);
+	vport->netdev = netdev;
+	vport->id = id;
+	vport->tx_packets = 0;
+	vport->tx_bytes = 0;
+	vport->tx_errors = 0;
+	vport->rx_packets = 0;
+	vport->rx_bytes = 0;
+	vport->rx_errors = 0;
+
+    spin_lock_init(&vport->lock);
+   	INIT_WORK(&vport->work, transmission_data);
 
 	eth_hw_addr_random(netdev);
-
+	
 	netdev->if_port = IF_PORT_10BASET;
 	netdev->netdev_ops = &netdev_ops;
 	netdev->watchdog_timeo = HZ;
@@ -136,33 +128,39 @@ static struct virtual_eth_port *alloc_and_init_device(unsigned int id)
 	netdev->max_mtu = 1500;
 	netdev->mtu	= 1500;
 
-	//netif_napi_add(netdev, &dev->napi, napi_poll, 64);
 	if (register_netdev(netdev)) {
 		free_netdev(netdev);
 		return NULL;
 	}
 
 	netif_start_queue(netdev);
-	return dev;
+	return vport;
 }
 
-static void destroy_and_deinit_port(struct virtual_eth_port *dev)
+static void destroy_and_deinit_port(struct virtual_eth_port *vport)
 {
-	unregister_netdev(dev->netdev);
-	free_netdev(dev->netdev);
+	unregister_netdev(vport->netdev);
+	free_netdev(vport->netdev);
 }
 
-static void link_dev_port(struct virtual_eth_port *dev0,struct virtual_eth_port *dev1)
+static void link_dev_port(struct virtual_eth_port *vport0,struct virtual_eth_port *vport1)
 {
-	dev0->partner_dev = dev1;
-	dev1->partner_dev = dev0;
+	vport0->partner_dev = vport1;
+	vport1->partner_dev = vport0;
 }
 
 static int __init veth_init(void)
 {
-	pr_info("virt eth port driver initialized\n");
+	pr_info("virt ethernet port driver initialized\n");
 	vport[0] = alloc_and_init_device(0);
+	if(vport[0] == NULL) {
+		return -1;
+	}
 	vport[1] = alloc_and_init_device(1);
+	if(vport[0] == NULL) {
+    	destroy_and_deinit_port(vport[0]);
+		return -1;
+	}
 	link_dev_port(vport[0],vport[1]);
 	return 0;
 }
