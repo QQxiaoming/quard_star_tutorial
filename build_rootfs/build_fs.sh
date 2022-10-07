abdb062e10c9fd26d8a3a47495108ee53a2acb83
xiaoming <2014500726@smail.xtu.edu.cn>
2022-10-07 11:38:27 +0800

    build:opt build rootfs script
    Signed-off-by: xiaoming <2014500726@smail.xtu.edu.cn>

--- build_rootfs/build_fs.sh
+++ build_rootfs/build_fs.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-losetup -o 0 --sizelimit 2147483648 /dev/loop70 $1/rootfs.img -P
+LOOPDEV="$(losetup -f)"
+losetup -o 0 --sizelimit 2147483648 $LOOPDEV $1/rootfs.img -P
 if [ -d "$1/target" ]; then  
 rm -rf $1/target
 fi
 mkdir $1/target
 mkdir $1/target/bootfs
 mkdir $1/target/rootfs
-mount /dev/loop70p1 $1/target/bootfs 
-mount /dev/loop70p2 $1/target/rootfs
+mount "$LOOPDEV"p1 $1/target/bootfs 
+mount "$LOOPDEV"p2 $1/target/rootfs
 
 cp -r $1/bootfs/* $1/target/bootfs/
 cp -r $1/rootfs/* $1/target/rootfs/
@@ -17,5 +18,5 @@ sleep 5
 
 umount $1/target/bootfs 
 umount $1/target/rootfs
-losetup -d /dev/loop70  
+losetup -d $LOOPDEV
 sync
