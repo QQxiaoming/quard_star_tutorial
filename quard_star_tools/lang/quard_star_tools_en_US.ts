14c3c566d3c44187cd550e2145608aec62ffad9d
xiaoming <2014500726@smail.xtu.edu.cn>
2022-11-05 23:12:48 +0800

    gui_tools:add help windows
    Signed-off-by: xiaoming <2014500726@smail.xtu.edu.cn>

--- quard_star_tools/lang/quard_star_tools_en_US.ts
+++ quard_star_tools/lang/quard_star_tools_en_US.ts
@@ -9,41 +9,65 @@
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.cpp" line="346"/>
-        <location filename="../boardview/boardwindow.cpp" line="353"/>
-        <location filename="../boardview/boardwindow.cpp" line="369"/>
+        <location filename="../boardview/boardwindow.cpp" line="350"/>
+        <location filename="../boardview/boardwindow.cpp" line="357"/>
+        <location filename="../boardview/boardwindow.cpp" line="373"/>
         <source>About</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.cpp" line="184"/>
+        <location filename="../boardview/boardwindow.cpp" line="185"/>
         <source>Error</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.cpp" line="184"/>
+        <location filename="../boardview/boardwindow.cpp" line="185"/>
         <source>power up error!</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.cpp" line="335"/>
-        <location filename="../boardview/boardwindow.cpp" line="342"/>
+        <location filename="../boardview/boardwindow.cpp" line="256"/>
+        <source>boot:
+ 000-pflash.
+ 001-spi_nor_flash.
+ 010-sd.
+ 100-uart0.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../boardview/boardwindow.cpp" line="336"/>
+        <location filename="../boardview/boardwindow.cpp" line="343"/>
         <source>Help</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.cpp" line="354"/>
+        <location filename="../boardview/boardwindow.cpp" line="344"/>
+        <source>1. Move the mouse over the component to explore.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../boardview/boardwindow.cpp" line="345"/>
+        <source>2. Right-click the component to view the settings.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../boardview/boardwindow.cpp" line="346"/>
+        <source>3. Double-click the component to enter the interface.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../boardview/boardwindow.cpp" line="358"/>
         <source>&lt;p&gt;Version&lt;/p&gt;&lt;p&gt;&amp;nbsp;%1&lt;/p&gt;&lt;p&gt;Commit&lt;/p&gt;&lt;p&gt;&amp;nbsp;%2&lt;/p&gt;&lt;p&gt;Author&lt;/p&gt;&lt;p&gt;&amp;nbsp;qiaoqm@aliyun.com&lt;/p&gt;&lt;p&gt;Website&lt;/p&gt;&lt;p&gt;&amp;nbsp;&lt;a href=&apos;https://github.com/QQxiaoming/quard_star_tutorial&apos;&gt;https://github.com/QQxiaoming&lt;/p&gt;&lt;p&gt;&amp;nbsp;&lt;a href=&apos;https://gitee.com/QQxiaoming/quard_star_tutorial&apos;&gt;https://gitee.com/QQxiaoming&lt;/a&gt;&lt;/p&gt;</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.cpp" line="203"/>
-        <location filename="../boardview/boardwindow.cpp" line="267"/>
+        <location filename="../boardview/boardwindow.cpp" line="204"/>
+        <location filename="../boardview/boardwindow.cpp" line="268"/>
         <source>Get Info</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.cpp" line="217"/>
+        <location filename="../boardview/boardwindow.cpp" line="218"/>
         <source>Quard Star SOC:
  8 core riscv64 architecture. 
 Internal packaging:
@@ -53,211 +77,205 @@ Support:
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.cpp" line="220"/>
+        <location filename="../boardview/boardwindow.cpp" line="221"/>
         <source>DDR:
  1G.</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.cpp" line="223"/>
+        <location filename="../boardview/boardwindow.cpp" line="224"/>
         <source>Nor Flash:
  is25wp256 32M.</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.cpp" line="226"/>
+        <location filename="../boardview/boardwindow.cpp" line="227"/>
         <source>NAND Flash:
  onenand 256M.</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.cpp" line="229"/>
+        <location filename="../boardview/boardwindow.cpp" line="230"/>
         <source>SD card:
  SDSC 32M.</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.cpp" line="232"/>
+        <location filename="../boardview/boardwindow.cpp" line="233"/>
         <source>USB Flash:
  32M, Speed 5000 Mb/s.</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.cpp" line="235"/>
+        <location filename="../boardview/boardwindow.cpp" line="236"/>
         <source>USB Serial:
  FT232RL, Speed 12 Mb/s.</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.cpp" line="238"/>
+        <location filename="../boardview/boardwindow.cpp" line="239"/>
         <source>LCDC:
  VGA port.</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.cpp" line="243"/>
+        <location filename="../boardview/boardwindow.cpp" line="244"/>
         <source>UART:
  115200-8-n-1.</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.cpp" line="246"/>
+        <location filename="../boardview/boardwindow.cpp" line="247"/>
         <source>QEMU monitor:
  monitor terminal.</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.cpp" line="249"/>
+        <location filename="../boardview/boardwindow.cpp" line="250"/>
         <source>ETH:
  IEEE 802.3 100M full duplex.</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.cpp" line="252"/>
+        <location filename="../boardview/boardwindow.cpp" line="253"/>
         <source>IIS:
  wm8750.</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.cpp" line="255"/>
-        <source>boot:
- 000-pflash 001-spi_nor_flash 010-sd 100-uart0.</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../boardview/boardwindow.cpp" line="258"/>
+        <location filename="../boardview/boardwindow.cpp" line="259"/>
         <source>power:
  DC 12V.</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.cpp" line="261"/>
+        <location filename="../boardview/boardwindow.cpp" line="262"/>
         <source>switch:
  power switch.</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.cpp" line="274"/>
+        <location filename="../boardview/boardwindow.cpp" line="275"/>
         <source>Open FileSystem</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.cpp" line="283"/>
+        <location filename="../boardview/boardwindow.cpp" line="284"/>
         <source>Setting</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.cpp" line="380"/>
+        <location filename="../boardview/boardwindow.cpp" line="384"/>
         <source>Exit</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.cpp" line="506"/>
+        <location filename="../boardview/boardwindow.cpp" line="510"/>
         <source>Select SD IMG</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.cpp" line="509"/>
+        <location filename="../boardview/boardwindow.cpp" line="513"/>
         <source>Select NorFlash IMG</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.cpp" line="512"/>
+        <location filename="../boardview/boardwindow.cpp" line="516"/>
         <source>Select NandFlash IMG</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.cpp" line="515"/>
+        <location filename="../boardview/boardwindow.cpp" line="519"/>
         <source>Select PFlash IMG</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.cpp" line="518"/>
+        <location filename="../boardview/boardwindow.cpp" line="522"/>
         <source>Select USBFlash IMG</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.h" line="92"/>
+        <location filename="../boardview/boardwindow.h" line="93"/>
         <source>vga</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.h" line="93"/>
+        <location filename="../boardview/boardwindow.h" line="94"/>
         <source>uart2</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.h" line="94"/>
+        <location filename="../boardview/boardwindow.h" line="95"/>
         <source>uart1</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.h" line="95"/>
+        <location filename="../boardview/boardwindow.h" line="96"/>
         <source>uart0</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.h" line="96"/>
+        <location filename="../boardview/boardwindow.h" line="97"/>
         <source>power</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.h" line="97"/>
+        <location filename="../boardview/boardwindow.h" line="98"/>
         <source>soc</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.h" line="98"/>
+        <location filename="../boardview/boardwindow.h" line="99"/>
         <source>ddr</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.h" line="99"/>
+        <location filename="../boardview/boardwindow.h" line="100"/>
         <source>eth</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.h" line="100"/>
+        <location filename="../boardview/boardwindow.h" line="101"/>
         <source>usb0</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.h" line="101"/>
+        <location filename="../boardview/boardwindow.h" line="102"/>
         <source>usb1</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.h" line="102"/>
+        <location filename="../boardview/boardwindow.h" line="103"/>
         <source>nand</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.h" line="103"/>
+        <location filename="../boardview/boardwindow.h" line="104"/>
         <source>nor</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.h" line="104"/>
+        <location filename="../boardview/boardwindow.h" line="105"/>
         <source>sd</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.h" line="105"/>
+        <location filename="../boardview/boardwindow.h" line="106"/>
         <source>switch</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.h" line="106"/>
+        <location filename="../boardview/boardwindow.h" line="107"/>
         <source>jtag</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.h" line="107"/>
+        <location filename="../boardview/boardwindow.h" line="108"/>
         <source>audio</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../boardview/boardwindow.h" line="108"/>
+        <location filename="../boardview/boardwindow.h" line="109"/>
         <source>boot</source>
         <translation></translation>
     </message>
