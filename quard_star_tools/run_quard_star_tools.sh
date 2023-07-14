830006f7972eba759bfdac00de994936ba96ad43
xiaoming <2014500726@smail.xtu.edu.cn>
2023-07-14 17:05:06 +0800

    script:update
    Signed-off-by: xiaoming <2014500726@smail.xtu.edu.cn>

--- quard_star_tools/run_quard_star_tools.sh
+++ quard_star_tools/run_quard_star_tools.sh
@@ -7,4 +7,4 @@ export QT_HOME=$SHELL_FOLDER/gui_tools/quard_star_tools
 export QT_PLUGIN_PATH=$QT_HOME/plugins
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$QT_HOME/lib" 
 
-$SHELL_FOLDER/gui_tools/quard_star_tools/quard_star_tools -e $SHELL_FOLDER/output
+$SHELL_FOLDER/gui_tools/quard_star_tools/bin/quard_star_tools -e $SHELL_FOLDER/output
