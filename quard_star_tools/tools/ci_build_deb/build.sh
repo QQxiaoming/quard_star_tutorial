f061fa8b0b72c23ed86fb87fc1e9426ba166f667
xiaoming <2014500726@smail.xtu.edu.cn>
2023-07-14 11:50:10 +0800

    gui tools:update build tools
    Signed-off-by: xiaoming <2014500726@smail.xtu.edu.cn>

--- quard_star_tools/tools/ci_build_deb/build.sh
+++ quard_star_tools/tools/ci_build_deb/build.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
-./linuxdeployqt ../../release/out/quard_star_tools_temp -appimage
-rm -rf ../../release/out/default.desktop
-rm -rf ../../release/out/default.png
-rm -rf ../../release/out/quard_star_tools_temp
-rm -rf ../../release/out/AppRun
-rm -rf ../../release/out/doc
-
+./linuxdeploy-x86_64.AppImage --executable=../../release/out/quard_star_tools_temp --appdir=../../release/out --plugin=qt
+rm -rf ../../release/out/apprun-hooks
